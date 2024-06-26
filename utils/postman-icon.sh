784013f60fad6ba20fb3f1fe25b8236c3ead6f48
Paolo Missagia <login@paolomissagia.com>
2024-06-26 20:26:29 +0100

    added postman

---
+++ utils/postman-icon.sh
@@ -0,0 +1,10 @@
+sudo tee /usr/share/applications/postman.desktop > /dev/null << 'EOF'
+[Desktop Entry]
+Type=Application
+Name=Postman
+Icon=/opt/Postman/app/resources/app/assets/icon.png
+Exec="/opt/Postman/Postman"
+Comment=Postman Desktop App
+Categories=Development;Code;
+EOF
+
