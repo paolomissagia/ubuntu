6d22f46d49460fbb7cd30fc6989d30bf9153c265
Paolo Missagia <login@paolomissagia.com>
2024-06-27 11:49:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 sudo apt update && sudo apt upgrade -y && sudo snap refresh
 
 sudo apt install -y \
-	 git curl libssl-dev libreadline-dev zlib1g-dev \
+	 git curl ripgrep libssl-dev libreadline-dev zlib1g-dev \
 	 autoconf bison build-essential libyaml-dev libreadline-dev \
-	 libncurses5-dev libffi-dev libgdbm-dev ripgrep libbz2-dev
+	 libncurses5-dev libffi-dev libgdbm-dev
 
 for script in ~/code/ubuntu/apps/*.sh; do source $script; done
 for script in ~/code/ubuntu/config/*.sh; do source $script; done
