704233a22650a26f2997d4f498f6ce5d2c617b6d
Paolo Missagia <login@paolomissagia.com>
2024-06-30 23:32:37 +0100

    added eza library to replace ls

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 sudo apt update && sudo apt upgrade -y && sudo snap refresh
 
 sudo apt install -y \
-	 git curl ripgrep unzip cmake libssl-dev libreadline-dev zlib1g-dev \
+	 git curl ripgrep unzip cmake eza libssl-dev libreadline-dev zlib1g-dev \
 	 autoconf bison build-essential libyaml-dev libreadline-dev \
 	 libncurses5-dev libffi-dev libgdbm-dev
 
