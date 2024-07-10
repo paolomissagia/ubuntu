baa0eefd117948aa73a9d4b2b8079cdf427e4641
Paolo Missagia <login@paolomissagia.com>
2024-07-10 08:42:02 +0100

    updated scripts

--- install/tmux.sh
+++ install/tmux.sh
@@ -1,4 +1,4 @@
 sudo apt install -y tmux
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/christoomey/vim-tmux-navigator ~/.tmux/vim-tmux-navigator
 
--- shell.sh
+++ shell.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-# Define the content to be added
-content=$(cat << 'EOF'
-# starship
-eval "$(starship init zsh)"
-
-# mise
-eval "$(~/.local/bin/mise activate zsh)"
-
-# nvim
-export PATH="$PATH:/opt/nvim-linux64/bin"
-
-# zsh plugins
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# aliases
-alias ls="eza -lha --group-directories-first --icons"
-EOF
-)
+# Variables for file paths
+ZSHRC=~/.zshrc
+TMUX_CONF=~/.config/tmux/tmux.conf
 
 # Check if .zshrc exists, create if not
-if [ ! -f ~/.zshrc ]; then
-    touch ~/.zshrc
+if [ ! -f "$ZSHRC" ]; then
+    touch "$ZSHRC"
 fi
 
-# Append content to .zshrc
-echo "$content" >> ~/.zshrc
+# Add configurations to .zshrc
+{
+    echo 'eval "$(starship init zsh)"'
+    echo 'eval "$(~/.local/bin/mise activate zsh)"'
+    echo 'export PATH="$PATH:/opt/nvim-linux64/bin"'
+    echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh'
+    echo 'source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
+    echo 'alias ls="eza -lha --group-directories-first --icons"'
+} >> "$ZSHRC"
+
+# Ensure tmux config directory exists
+mkdir -p ~/.config/tmux
+
+# Check if tmux.conf exists, create if not
+if [ ! -f "$TMUX_CONF" ]; then
+    touch "$TMUX_CONF"
+fi
+
+# Add configuration to tmux.conf
+echo 'run-shell ~/.tmux/vim-tmux-navigator/vim-tmux-navigator.tmux' >> "$TMUX_CONF"
