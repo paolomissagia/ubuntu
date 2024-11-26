76f3f6d2275c4b1e7a27b3630f4146b7ff282f5f
Paolo Missagia <login@paolomissagia.com>
2024-11-26 07:14:50 +0000

    added run logic

--- run.sh
+++ run.sh
@@ -2,4 +2,12 @@ sudo apt update -y && sudo apt upgrade -y
 
 sudo snap refresh -y
 
-sudo apt install -y git tmux curl ca-certificates stow gpg
+sudo apt install -y git tmux curl ca-certificates stow gpg build-essential
+
+for install in ~/code/ubuntu/installs/**/*.sh; do source $install; done
+
+for config in ~/code/ubuntu/installs/*.sh; do source $config; done
+
+for util in ~/code/ubuntu/utils/*.sh; do source $util; done
+
+for icon in ~/code/ubuntu/icons/*.sh; do source $icon; done
