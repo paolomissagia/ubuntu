de033627ff01878ccc5db6483c8eb13f82965b8d
Paolo Missagia <login@paolomissagia.com>
2025-01-03 10:50:46 +0000

    added starship

---
+++ terminal/starship.sh
@@ -0,0 +1,7 @@
+cd /tmp
+
+wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz
+
+sudo tar -C /usr/bin -xzf starship-x86_64-unknown-linux-gnu.tar.gz
+
+cd -
