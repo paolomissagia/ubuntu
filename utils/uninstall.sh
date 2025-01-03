30a8a6d6e357fbaf6915571c5365b1c72bf4a992
Paolo Missagia <login@paolomissagia.com>
2025-01-03 11:52:30 +0000

    added file to clean up stuff

---
+++ utils/uninstall.sh
@@ -0,0 +1,6 @@
+sudo apt remove -y command-not-found
+
+sudo snap remove --purge firefox
+
+sudo rm /usr/shared/applications/org.gnome.Terminal.desktop
+sudo rm /usr/shared/applications/org.gnome.Terminal.Preferences.desktop
