8fa043929746ed741d368444fc28daaf61881e9d
Paolo Missagia <login@paolomissagia.com>
2025-01-03 11:58:12 +0000

    added rbenv dep to libraries install

--- libraries.sh
+++ libraries.sh
@@ -1,11 +1,15 @@
-# for ppa
+# ppa
 sudo add-apt-repository universe -y
 
-# for private repos
+# private repos
 sudo install -dm 755 /etc/apt/keyrings
 
 # dependencies
 sudo apt install -y curl
 
-# for anki
+# anki
 sudo apt install -y libxcb-xinerama0 libxcb-cursor0 libnss3
+
+# rbenv
+sudo apt install -y libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential \
+		    libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
