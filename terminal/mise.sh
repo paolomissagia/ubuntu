6e53fd3970951eff9f06e9f98ce2dd99de8f0b0d
Paolo Missagia <login@paolomissagia.com>
2025-01-03 10:39:33 +0000

    install mise

---
+++ terminal/mise.sh
@@ -0,0 +1,7 @@
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+
+sudo apt update -y
+
+sudo apt install -y mise
