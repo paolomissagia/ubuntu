cdba4ccfa6af86863b487c2308107859c5b95103
Paolo Missagia <login@paolomissagia.com>
2025-01-03 10:30:02 +0000

    added ghostty installer

---
+++ ghostty.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://github.com/mkasberg/ghostty-ubuntu/releases/download/1.0.1-0-ppa1/ghostty_1.0.1-0.ppa1_amd64_24.10.deb
+sudo apt install -y ./ghostty_1.0.1-0.ppa1_amd64_24.10.deb
+rm ghostty_1.0.1-0.ppa1_amd64_24.10.deb
+cd -
