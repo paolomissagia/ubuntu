40463dfb05d771ce1e13868ccd5f682d24199204
Paolo Missagia <login@paolomissagia.com>
2025-01-19 23:07:12 +0000

    fixed typo on folder name

--- utils/uninstall.sh
+++ utils/uninstall.sh
@@ -2,5 +2,5 @@ sudo apt remove -y command-not-found
 
 sudo snap remove --purge firefox
 
-sudo rm /usr/shared/applications/org.gnome.Terminal.desktop
-sudo rm /usr/shared/applications/org.gnome.Terminal.Preferences.desktop
+sudo rm /usr/share/applications/org.gnome.Terminal.desktop
+sudo rm /usr/share/applications/org.gnome.Terminal.Preferences.desktop
