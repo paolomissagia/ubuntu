13d5672fcb0c1bdfc39383b10e8c033aac499104
Paolo Missagia <login@paolomissagia.com>
2025-02-17 03:43:42 +0000

    update libraries installation

--- libraries.sh
+++ libraries.sh
@@ -5,11 +5,4 @@ sudo add-apt-repository universe -y
 sudo install -dm 755 /etc/apt/keyrings
 
 # dependencies
-sudo apt install -y curl unzip
-
-# anki
-sudo apt install -y libxcb-xinerama0 libxcb-cursor0 libnss3
-
-# rbenv
-sudo apt install -y libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential \
-		    libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+sudo apt install -y curl unzip build-essential ripgrep
