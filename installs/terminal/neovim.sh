c6bf948ba9b6bc761bf7c1e2fe398ff65d4df254
Paolo Missagia <login@paolomissagia.com>
2025-02-17 03:43:52 +0000

    updated neovim installation

--- installs/terminal/neovim.sh
+++ installs/terminal/neovim.sh
@@ -1,7 +1,7 @@
 cd /tmp
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
 cd -
