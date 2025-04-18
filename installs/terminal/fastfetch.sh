7e88488ca7ec0a6baf7c798746967a644601c4ca
Paolo Missagia <login@paolomissagia.com>
2025-04-19 00:14:31 +0100

    updated install libraries

--- installs/terminal/fastfetch.sh
+++
@@ -1,5 +0,0 @@
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
-
-sudo apt update -y
-
-sudo apt install -y fastfetch
--- libraries.sh
+++ libraries.sh
@@ -5,7 +5,16 @@ sudo apt update && sudo apt upgrade && sudo snap refresh
 sudo add-apt-repository universe -y
 
 # private repos
-sudo install -dm 755 /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 
 # dependencies
-sudo apt install -y curl unzip build-essential ripgrep fd-find
+sudo apt install -y curl build-essential
+
+# neovim
+sudo apt install -y ripgrep fd-find unzip
+
+# docker
+sudo apt install -y ca-certificates
+
+# nordvpn
+sudo apt install -y apt-transport-https
