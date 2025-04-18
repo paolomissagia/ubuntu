54bb19d18171874c7074325703de57bb9e943614
Paolo Missagia <login@paolomissagia.com>
2025-04-19 00:13:29 +0100

    update chrome

--- icons/google-chrome.sh
+++ icons/google-chrome.sh
@@ -2,7 +2,7 @@ cat <<EOF >~/.local/share/applications/google-chrome.desktop
 [Desktop Entry]
 Name=Google Chrome
 Comment=Access the Internet
-Exec=/usr/bin/google-chrome-stable --enable-features=TouchpadOverscrollHistoryNavigation %U
+Exec=/usr/bin/google-chrome-stable --enable-features=TouchpadOverscrollHistoryNavigation --disable-features=WaylandFractionalScaleV1 %U
 Icon=google-chrome
 Terminal=false
 Type=Application
