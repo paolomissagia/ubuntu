4b98c2bea504af03131777bea593b399d8fd4cf2
Paolo Missagia <login@paolomissagia.com>
2025-04-19 00:13:40 +0100

    update ghostty for new version

--- installs/desktop/ghostty.sh
+++ installs/desktop/ghostty.sh
@@ -1,5 +1,5 @@
 cd /tmp
-wget https://github.com/mkasberg/ghostty-ubuntu/releases/download/1.1.0-0-ppa1/ghostty_1.1.0-0.ppa1_amd64_24.10.deb
-sudo apt install -y ./ghostty_1.1.0-0.ppa1_amd64_24.10.deb
-rm ghostty_1.1.0-0.ppa1_amd64_24.10.deb
+wget https://github.com/mkasberg/ghostty-ubuntu/releases/download/1.1.3-0-ppa1/ghostty_1.1.3-0.ppa1_amd64_24.10.deb
+sudo apt install -y ./ghostty_1.1.3-0.ppa1_amd64_24.10.deb
+rm ghostty_1.1.3-0.ppa1_amd64_24.10.deb
 cd -
