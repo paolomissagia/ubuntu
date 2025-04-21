74701e76e2e3a322f4cb5f3281446d415ee39006
Paolo Missagia <login@paolomissagia.com>
2025-04-21 14:38:20 +0100

    fixed nordvpn install

--- installs/desktop/nordvpn.sh
+++ installs/desktop/nordvpn.sh
@@ -1,4 +1,4 @@
-wget -qO - https://repo.nordvpn.com/gpg/nordvpn_public.asc | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+wget -qO - https://repo.nordvpn.com/gpg/nordvpn_public.asc | sudo tee /etc/apt/trusted.gpg.d/nordvpn_public.asc > /dev/null
 
 echo "deb https://repo.nordvpn.com/deb/nordvpn/debian stable main" | sudo tee /etc/apt/sources.list.d/nordvpn.list
 
