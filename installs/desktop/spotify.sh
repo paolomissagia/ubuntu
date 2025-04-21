6e27c4c2be200c8ebd70880cde90d3436dca45c1
Paolo Missagia <login@paolomissagia.com>
2025-04-21 14:38:28 +0100

    moved spotify to https

--- installs/desktop/spotify.sh
+++ installs/desktop/spotify.sh
@@ -1,6 +1,6 @@
 curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt update -y
 
