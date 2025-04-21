1738136b00a0bdc4e248248c3b9ec924db4be745
Paolo Missagia <login@paolomissagia.com>
2025-04-21 16:40:02 +0100

    added vscode install

---
+++ installs/desktop/vscode.sh
@@ -0,0 +1,7 @@
+cd /tmp
+
+wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/17baf841131aa23349f217ca7c570c76ee87b957/code_1.99.3-1744761595_amd64.deb
+
+sudo apt install -y ./code_1.99.3-1744761595_amd64.deb
+
+cd -
