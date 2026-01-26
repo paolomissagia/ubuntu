35cdf708e28d95a53b10dc0e7177253230dd138e
Paolo Missagia <hello+5xnavgxx@paolomissagia.com>
2026-01-26 21:01:03 +0100

    feat: added anki install

---
+++ installs/desktop/anki.sh
@@ -0,0 +1,11 @@
+cd /tmp
+
+wget https://github.com/ankitects/anki/releases/download/25.09/anki-launcher-25.09-linux.tar.zst
+
+tar xaf anki-launcher-**.**-linux.tar.zst
+
+cd anki-launcher-**.**-linux
+
+sudo ./install.sh
+
+cd -
