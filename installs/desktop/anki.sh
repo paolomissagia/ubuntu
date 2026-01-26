01afbcc2642cb8f5fb19037b39ede7abba819279
Paolo Missagia <hello+5xnavgxx@paolomissagia.com>
2026-01-26 21:02:19 +0100

    chore: added version number to script command

--- installs/desktop/anki.sh
+++ installs/desktop/anki.sh
@@ -2,9 +2,9 @@ cd /tmp
 
 wget https://github.com/ankitects/anki/releases/download/25.09/anki-launcher-25.09-linux.tar.zst
 
-tar xaf anki-launcher-**.**-linux.tar.zst
+tar xaf anki-launcher-25.09-linux.tar.zst
 
-cd anki-launcher-**.**-linux
+cd anki-launcher-25.09-linux
 
 sudo ./install.sh
 
