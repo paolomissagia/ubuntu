8b2ea237f1c2c875f2bd38cf8aa84810bdad4810
Paolo Missagia <hello@paolomissagia.com>
2026-07-01 09:20:41 +0200

    feat: update mise installer

--- installs/terminal/mise.sh
+++ installs/terminal/mise.sh
@@ -1,6 +1,4 @@
-wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
-
-echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo extrepo enable mise
 
 sudo apt update -y
 
