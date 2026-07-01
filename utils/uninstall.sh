d024f15150b60dcd38767cb503b979f945bcccd3
Paolo Missagia <hello@paolomissagia.com>
2026-07-01 09:21:00 +0200

    feat: updated uninstall script

--- utils/uninstall.sh
+++ utils/uninstall.sh
@@ -1,4 +1,5 @@
 sudo apt remove -y command-not-found
 
-sudo rm /usr/share/applications/org.gnome.Terminal.desktop
-sudo rm /usr/share/applications/org.gnome.Terminal.Preferences.desktop
+sudo snap remove --purge firefox
+
+sudo rm /usr/share/applications/org.gnome.Ptyxis.desktop
