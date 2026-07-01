0998497c03cf2fb8d292be1b5a2fcd7e503e5b42
Paolo Missagia <hello@paolomissagia.com>
2026-07-01 09:20:22 +0200

    feat: update spotify installer

--- installs/desktop/spotify.sh
+++ installs/desktop/spotify.sh
@@ -1,4 +1,4 @@
-curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 
 echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
