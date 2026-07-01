b1d4787d4e3d39d7e119b1481dfa96a9a16e5f2f
Paolo Missagia <hello@paolomissagia.com>
2026-07-01 09:20:45 +0200

    feat: udpate neovim installer

--- installs/terminal/neovim.sh
+++ installs/terminal/neovim.sh
@@ -1,7 +1,1 @@
-cd /tmp
-
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-
-cd -
+sudo apt install -y neovim
